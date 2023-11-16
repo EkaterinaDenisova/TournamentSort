28add32ee4cdad652afa55b4d62d08be1bccead1
EkaterinaDenisova <ekaterina_denisova24@inbox.ru>
2023-11-16 17:43:01 +0900

    added more tests

--- ConsoleApplication_TournamentSort/Test_TournamentSort/test.cpp
+++ ConsoleApplication_TournamentSort/Test_TournamentSort/test.cpp
@@ -18,8 +18,48 @@ TEST(TestSort, TestTournamentSort) {
 	}
 
     EXPECT_EQ(v, v1);
+	v.clear(); v1.clear();
   
 
+	// массив из одного элемента
+	int arr1[] = { -2 };
+	arrLength = 1;
 
+	tournamentSort(arr1, arrLength);
+	v = { -2 };
+	for (int i = 0; i < arrLength; i++) {
+		v1.push_back(arr1[i]);
+	}
+
+	EXPECT_EQ(v, v1);
+	v.clear(); v1.clear();
+
+
+	// пустой массив
+	int* arr2 = nullptr;
+	arrLength = 0;
+
+	tournamentSort(arr2, arrLength);
+	EXPECT_EQ(arr2,nullptr);
+	v = { };
+	for (int i = 0; i < arrLength; i++) {
+		v1.push_back(arr2[i]);
+	}
+
+	EXPECT_EQ(v, v1);
+	v.clear(); v1.clear();
+
+	// массив со значениями max_int и min_int
+	int arr3[] = {7, 38, 89, INT_MAX, 165, 56, -2, INT_MIN, -31, 648, 5125, 5126};
+	arrLength = 12;
+
+	tournamentSort(arr3, arrLength);
+	v = { INT_MIN, -31, -2, 7, 38, 56, 89, 165,  648, 5125, 5126, INT_MAX};
+	for (int i = 0; i < arrLength; i++) {
+		v1.push_back(arr3[i]);
+	}
+
+	EXPECT_EQ(v, v1);
+	v.clear(); v1.clear();
 
 }
